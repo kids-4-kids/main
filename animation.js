6c2ed438ff724785d8779501626a45b90514eb25
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-01-28 19:03:49 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -5,15 +5,16 @@ function init() {
   var bodyId = document.getElementById("body")
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
-  var x = -canvas.width;
-  var y = canvas.height;
+  var x = canvas.width;
+  var y = 0;
 
   bodyId.onload = animate;
   console.log("success");
   function animate() {
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
     ctx.drawImage(imgTag, x, y);                       // draw image at current position
-    x += 4;
+    x -= 4;
     if (x > canvas.width) requestAnimationFrame(animate)        // loop Never loop forever other wise u get expelled
 
   }
