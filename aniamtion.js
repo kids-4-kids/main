d21a5406d3f30a8ac94776710df1fa42c44573dd
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-01-28 18:17:50 +0100

    Update aniamtion.js

--- aniamtion.js
+++ aniamtion.js
@@ -1,7 +1,15 @@
+var image = document.getElementById("img")
+var bodyId = document.getElementById("body")
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var x = -canvas.width;
+var y = canvas.height;
 
+bodyId.onload = animate;
 
-function frame1() {
-  
-};
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
+  ctx.drawImage(imgTag, x, y);                       // draw image at current position
+  x += 4;
+  if (x > canvas.width) requestAnimationFrame(animate)        // loop
+}
