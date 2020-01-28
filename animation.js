4d480f50bf0a21246a39d3981fc09957c05686cd
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-01-28 19:28:30 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -13,7 +13,7 @@ function init() {
   function animate() {
     
     ctx.clearRect(0, 0, canvas.width, canvas.height);  // clear canvas
-    ctx.drawImage(imgTag, x, y);                       // draw image at current position
+    ctx.drawImage(image, x, y);                       // draw image at current position
     x -= 4;
     if (x > canvas.width) requestAnimationFrame(animate)        // loop Never loop forever other wise u get expelled
 
