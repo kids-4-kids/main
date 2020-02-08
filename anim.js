83b3536dce9844bea9c233cbbc93d73b8fb2a127
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-02-08 21:13:26 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -1,17 +1,22 @@
-var can = document.getElementById("canvas");
-var image = document.getElementById("img");
-var ctx = can.getContext("2d");
+window.onload = body.onload = Init();
 
-var xpos = -image.width;
-window.onload = move();
-function move() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+function Init() {
 
-  if (image.right < screen.width/2) {
-      xpos+=1;
-      ctx.drawImage(image, 10, 10, image.width, image.height);
-      setTimeout(move, 100);
+  var can = document.getElementById("canvas");
+  var image = document.getElementById("img");
+  var ctx = can.getContext("2d");
+
+  var xpos = -image.width;
+  move();
+  function move() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      if (image.right < screen.width/2) {
+          xpos+=1;
+          ctx.drawImage(image, 10, 10, image.width, image.height);
+          setTimeout(move, 100);
+      }
+
+
   }
-  
-  
 }
