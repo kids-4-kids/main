cd935bdfa0d7a2345aca9886c5951f4b9efe1f82
itay-cyber <57634628+itay-cyber@users.noreply.github.com>
2020-02-14 07:58:34 +0100

    Update checkblank.js

--- checkblank.js
+++ checkblank.js
@@ -1,1 +1,12 @@
 
+      function checkblank() {
+        var body = document.getElementById("body");
+        body.onload = "checkblank();";
+        var blank = document.getElementById("blank");
+        if (blank.getAttribute("id") == "blank") {
+          var para = document.getElementById("isblank");
+          para.innerHTML = "if we have any upcoming events, they will be here!";
+        }
+      }
+      
+ 
