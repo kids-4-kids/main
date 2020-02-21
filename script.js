286d8d9ccb1c82d11e84ee1978e9f591d509b152
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-02-21 15:45:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 var winSize = window.matchMedia("(max-width: 1024px)")
+
 if (winSize.matches) {
-  mobile();
-  }
   
+  mobile();
+  
+  }
+else {
+  
+  nonMobile();
+  
+}
+
+
+function mobile() {
+  var Nobile = document.getElementsByClassName("not_mobile");
+  var isMobile = document.getElementsByClassName("mobile");
+  Nobile.style.display = "none";
+  isMobile.style.display = "block";
+}
+
+
+function nonMobile();
+  var Nobile = document.getElementsByClassName("not_mobile");
+  var isMobile = document.getElementsByClassName("mobile");
+  Nobile.style.display = "block";
+  isMobile.style.display = "none";
 
   
