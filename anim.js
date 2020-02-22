820f7d4cca14f8a707c80835343ab3f86c2a7f98
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-02-22 14:20:23 +0100

    Update anim.js

--- anim.js
+++ anim.js
@@ -2,5 +2,3 @@ var menu = document.getElementsByClassName("menubar");
 var tl = new TimelineMax();
 tl.fromTo(menu,1,{width: "0%"},{width: "100%"});
 
-function changeColor(object) {
-  tl.fromTo(object,1,{color: "blue"},{color: "red"});
